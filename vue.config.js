8b41bbb4524c3c85e9257675fa1fa06a77cf6b0b
Pranav <absolutelynoclutch@gmail.com>
2021-01-31 09:54:02 -0800

    Fix history

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports =
   devServer: {
     proxy: {
         '^/api': {
-            target: 'http://localhost:5000',
+            target: 'https://flashme-backend.herokuapp.com',
             changeOrigin: true,
             secure: false,
         },
